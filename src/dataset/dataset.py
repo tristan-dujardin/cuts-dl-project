a424b83109573cfd2e6c12d3e6478d3f8b91d801
Tristan Dujardin <tristan.dujardin@epita.fr>
2024-11-26 13:38:43 +0100

    feat: dataset download script

---
+++ src/dataset/dataset.py
@@ -0,0 +1,17 @@
+import os
+import zipfile
+from urllib import request
+
+url = "https://github.com/KrishnaswamyLab/CUTS/raw/main/data/retina.zip"
+data_path = "data/retina/"
+zip_path = "data/retina.zip"
+
+os.makedirs(os.path.dirname(data_path), exist_ok=True)
+
+
+request.urlretrieve(url, zip_path)
+
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(data_path)
+
+print(f"Files extracted to: {data_path}")
